effcdd817299dfae47d738fa05484ff2979341cb
jkelly90 <jlfkelly@gmail.com>
2020-08-25 16:32:06 +0200

    added update countdown function

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ const loading = document.getElementById('loading');
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown time
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
